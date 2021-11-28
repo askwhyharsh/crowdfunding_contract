c8cdf2ff0f215dd6a674917b5bc517136e9fb03d
askwhyharsh <askwhyharsh@gmail.com>
2021-11-28 10:41:13 +0530

    base contract for funding, refund and payout

--- contracts/crowdfunding.sol
+++ contracts/crowdfunding.sol
@@ -2,16 +2,135 @@
 pragma solidity ^0.8.0;
 
 import "hardhat/console.sol";
+import '@openzeppelin/contracts/utils/math/SafeMath.sol';
 
 
-
-contract crowdfunding {
+contract Project {
+    using SafeMath for uint256;
     
-    function getCaller() public view returns(address) {
-        return msg.sender;
-
+    // enum used for defining state of the funding
+    enum State {
+        Fundraising,
+        Expired,
+        Successful
     }
-    
+
+    // State variables
+    address payable public creator;
+    uint public amountGoal; // required to reach at least this much, else everyone gets refund
+    uint public completeAt;
+    uint256 public currentBalance;
+    uint public deadline;
+    string public title;
+    string public description;
+    State public state = State.Fundraising; // initialize on create
+    mapping (address => uint) public contributions;
+
+    // Event that will be emitted whenever funding will be received
+    event FundingReceived(address contributor, uint amount, uint currentTotal);
+    // Event that will be emitted whenever the project starter has received the funds
+    event CreatorPaid(address recipient);
+
+    // Modifier to check current state
+    modifier inState(State _state) {
+        require(state == _state);
+        _;
+    }
 
 
+    constructor
+    (
+        address payable projectStarter,
+        string memory projectTitle,
+        string memory projectDesc,
+        uint fundRaisingDeadline,
+        uint goalAmount
+    ) {
+        creator = projectStarter;
+        title = projectTitle;
+        description = projectDesc;
+        amountGoal = goalAmount;
+        deadline = fundRaisingDeadline;
+        currentBalance = 0;
+    }
+
+    /** Function to fund a certain project.
+      */
+    function contribute() external inState(State.Fundraising) payable {
+        require(msg.sender != creator);
+        contributions[msg.sender] = contributions[msg.sender].add(msg.value);
+        currentBalance = currentBalance.add(msg.value);
+        emit FundingReceived(msg.sender, msg.value, currentBalance);
+        checkIfFundingCompleteOrExpired();
+    }
+
+    /**  Function to change the project state depending on conditions.
+      */
+    function checkIfFundingCompleteOrExpired() public {
+        if (currentBalance >= amountGoal) {
+            state = State.Successful;
+            payOut();
+        } else if (block.timestamp > deadline)  {
+            state = State.Expired;
+        }
+        completeAt = block.timestamp;
+    }
+
+    /** Function to give the received funds to project starter.
+      */
+    function payOut() internal inState(State.Successful) returns (bool) {
+        uint256 totalRaised = currentBalance;
+        currentBalance = 0;
+
+        if (creator.send(totalRaised)) {
+            emit CreatorPaid(creator);
+            return true;
+        } else {
+            currentBalance = totalRaised;
+            state = State.Successful;
+        }
+
+        return false;
+    }
+
+
+    /** Function to retrieve donated amount when a project expires.
+      */
+    function getRefund() public inState(State.Expired) returns (bool) {
+        require(contributions[msg.sender] > 0);
+
+        uint amountToRefund = contributions[msg.sender];
+        contributions[msg.sender] = 0;
+         address payable sender = payable(msg.sender);
+        if (!sender.send(amountToRefund)) {
+            contributions[msg.sender] = amountToRefund;
+            return false;
+        } else {
+            currentBalance = currentBalance.sub(amountToRefund);
+        }
+
+        return true;
+    }
+
+    // /** Function to get specific information about the project.
+    //   * Returns all the project's details
+    //   */
+    function getDetails() public view returns 
+    (
+        address payable projectStarter,
+        string memory projectTitle,
+        string memory projectDesc,
+        uint256 deadLine,
+        State currentState,
+        uint256 currentAmount,
+        uint256 goalAmount
+    ) {
+        projectStarter = creator;
+        projectTitle = title;
+        projectDesc = description;
+        deadLine = deadline;
+        currentState = state;
+        currentAmount = currentBalance;
+        goalAmount = amountGoal;
+    }
 }
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -6,12 +6,15 @@ const hre = require("hardhat");
 
 async function main() {
 
-  const Crowdfunding = await hre.ethers.getContractFactory("crowdfunding");
-  const crowdfunding = await Crowdfunding.deploy();
+  const Crowdfunding = await hre.ethers.getContractFactory("Project");
+  const crowdfunding = await Crowdfunding.deploy("0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266", "NewProject", "test", 2, 100 );
 
   await crowdfunding.deployed();
 
   console.log("Crowdfunding project deployed to:", crowdfunding.address);
+  let txn = await crowdfunding.getDetails();
+   console.log(txn)
+
 }
 
 // We recommend this pattern to be able to use async/await everywhere
