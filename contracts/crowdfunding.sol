8e4c9b0f619b350fbc05f44c7db69a8334b5e196
askwhyharsh <askwhyharsh@gmail.com>
2021-12-15 01:46:28 +0530

    withdrawal fee of 3% added

--- contracts/crowdfunding.sol
+++ contracts/crowdfunding.sol
@@ -7,6 +7,7 @@ import '@openzeppelin/contracts/token/ERC721/ERC721.sol';
 import '@openzeppelin/contracts/utils/Counters.sol';
 import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 
+
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/math/SafeMath.sol";
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/utils/Counters.sol";
 // import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/ERC721.sol";
@@ -15,7 +16,12 @@ import '@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol';
 
 contract projectContract {
     using SafeMath for uint256;
-    
+    address payable public owner;
+
+    function changeOwner(address payable _newOwner) external {
+        require( payable(msg.sender) == owner, "only owner can change owner");
+        owner = _newOwner;
+    }
     // enum used for defining state of the funding
     enum State {
         Fundraising,
@@ -23,7 +29,7 @@ contract projectContract {
         Successful
     }
     struct Request {
-        
+        uint requestId;
         string desc; 
         uint value;
         address payable receipient; 
@@ -32,7 +38,7 @@ contract projectContract {
         mapping (address=>bool) voters;
     }
     struct RequestDetails {
-        
+        uint requestId;
         string desc; 
         uint value;
         address payable receipient; 
@@ -55,7 +61,7 @@ contract projectContract {
     State  state; // initialize on create  stae = State.Fundraising;
     mapping (address => uint)  contributions;
     uint noOfContributors;
-    mapping (uint => Request)  requests;
+    Request[] requests;
     uint  numRequests;
    
    }
@@ -153,8 +159,8 @@ function startProject(
     function contribute(uint _projectId) external payable returns(bool){
         require(msg.sender != projects[_projectId].creator, "Project creator can't contribute");
         checkIfFundingCompleteOrExpired(_projectId);
-        require( projects[_projectId].state == State.Fundraising, "project expired or succesful can't contribute");
-        projects[_projectId].contributions[msg.sender] = projects[_projectId].contributions[msg.sender].add(msg.value);
+        require( projects[_projectId].state == State.Fundraising, "expired or succesful"); 
+       
         projects[_projectId].currentBalance = projects[_projectId].currentBalance.add(msg.value);
         projects[_projectId].noOfContributors++;
 
@@ -162,10 +168,17 @@ function startProject(
         projectsR[_projectId].noOfContributors++;
         projectsR[_projectId].currentBalance = projectsR[_projectId].currentBalance.add(msg.value);
 
-
         // emit FundingReceived(msg.sender, msg.value, currentBalance);
+         if (projects[_projectId].contributions[msg.sender] == 0) {
         new FundNFT( payable (address (msg.sender)),  projects[_projectId].uri); 
-         checkIfFundingCompleteOrExpired(_projectId);
+        projects[_projectId].contributions[msg.sender] = projects[_projectId].contributions[msg.sender].add(msg.value);
+        checkIfFundingCompleteOrExpired(_projectId);
+         }
+         else {
+          projects[_projectId].contributions[msg.sender] = projects[_projectId].contributions[msg.sender].add(msg.value);
+          checkIfFundingCompleteOrExpired(_projectId);
+
+         }
         return true;
 
     }
@@ -177,13 +190,18 @@ function startProject(
             projects[_projectId].state = State.Successful;
 
             projectsR[_projectId].state = State.Successful;
-           
+            
 
-        // payOut();
+        
         } else if (block.timestamp > projects[_projectId].deadline)  {
             projects[_projectId].state = State.Expired;
             projectsR[_projectId].state = State.Expired;
           
+        }
+        else {
+             projects[_projectId].state = State.Fundraising;
+            projectsR[_projectId].state = State.Fundraising;
+
         }
        
     }
@@ -194,7 +212,7 @@ function startProject(
     function getRefund(uint _projectId) public returns (bool) {
          checkIfFundingCompleteOrExpired(_projectId);
         require( projects[_projectId].state == State.Expired, "project not expired, can't refund");
-        require(projects[_projectId].contributions[msg.sender] > 0);
+        require(projects[_projectId].contributions[msg.sender] > 0, "you have not contributed to this project");
 
 
         uint amountToRefund = projects[_projectId].contributions[msg.sender];
@@ -225,51 +243,49 @@ function startProject(
         require( projects[_projectId].state == State.Successful, "project expired or successful can't create request");
         require(msg.sender == projects[_projectId].creator, "only manager can create Request");
         require(_value <= projects[_projectId].currentBalance, "withdrawal is more than balance");
-        uint num = projects[_projectId].numRequests;
-        Request storage newRequest = projects[_projectId].requests[num];
+        projects[_projectId].requests.push();
         projectsR[_projectId].requestsDetails.push();
-        RequestDetails memory newRequestDetails = projectsR[_projectId].requestsDetails[num];
+        uint num = projects[_projectId].requests.length - 1;
+        projectsR[_projectId].requestsDetails[num];
+
+         projects[_projectId].requests[num].desc = _desc;
+         projects[_projectId].requests[num].value = _value;
+         projects[_projectId].requests[num].receipient = _receipient;
+         projects[_projectId].requests[num].requestId = num;
+
+        projectsR[_projectId].requestsDetails[num].desc = _desc;
+        projectsR[_projectId].requestsDetails[num].value = _value;
+        projectsR[_projectId].requestsDetails[num].receipient = _receipient;
+        projectsR[_projectId].requestsDetails[num].requestId = num;
+        
         projects[_projectId].numRequests++;
         projectsR[_projectId].numRequests++;
-
-        newRequest.desc = _desc;
-        newRequest.value = _value;
-        newRequest.receipient = _receipient;
-        newRequest.status = false;
-        newRequest.noOfVoter = 0;
-
-        newRequestDetails.desc = _desc;
-        newRequestDetails.value = _value;
-        newRequestDetails.receipient = _receipient;
-        newRequestDetails.status = false;
-        newRequestDetails.noOfVoter = 0;
 
     }
 
     // function to add vote to particular request 
-    function voteRequest(uint _projectId, uint _requestNo) public returns(bool, uint){
+    function voteRequest(uint _projectId, uint _requestNo) public {
          require( projects[_projectId].state == State.Successful, "project expired or successful can't create request");
         require(projects[_projectId].contributions[msg.sender] > 0, "you must be a contributor to vote");
 
-        Request storage thisRequest = projects[_projectId].requests[_requestNo];
-        RequestDetails storage thisRequestDetails = projectsR[_projectId].requestsDetails[_requestNo];
+        // Request storage thisRequest = projects[_projectId].requests[_requestNo];
+        // RequestDetails storage thisRequestDetails = projectsR[_projectId].requestsDetails[_requestNo];
 
-        require(thisRequest.noOfVoter < projects[_projectId].noOfContributors.div(2));
-        require (thisRequest.voters[msg.sender] == false, "you have already voted");
-        thisRequest.noOfVoter++;
-        thisRequestDetails.noOfVoter++;
+        require (projects[_projectId].requests[_requestNo].voters[msg.sender] == false, "you have already voted");
+        projects[_projectId].requests[_requestNo].noOfVoter++;
+        projectsR[_projectId].requestsDetails[_requestNo].noOfVoter++;
 
-        thisRequest.voters[msg.sender] = true;
+        projects[_projectId].requests[_requestNo].voters[msg.sender] = true;
         // return thisRequest.noOfVoter;
-        if(thisRequest.noOfVoter >= projects[_projectId].noOfContributors.div(2)) {
-        thisRequest.status = true;
-        thisRequestDetails.status = true;
-        sendPayoutRequest(_projectId, thisRequest.receipient, thisRequest.value, _requestNo);    
+        if(projects[_projectId].requests[_requestNo].noOfVoter*2 >= projects[_projectId].noOfContributors && projectsR[_projectId].requestsDetails[_requestNo].value <= projects[_projectId].currentBalance) {
+        projects[_projectId].requests[_requestNo].status = true;
+        projectsR[_projectId].requestsDetails[_requestNo].status = true;
+        sendPayoutRequest(_projectId, projects[_projectId].requests[_requestNo].receipient, projects[_projectId].requests[_requestNo].value, _requestNo);    
         }
         else {
+            
          
         }
-        return (false, thisRequest.noOfVoter);
         
     } 
     // function to send payout to particular address if the vote is won by creator
@@ -278,8 +294,11 @@ function startProject(
          Request storage thisRequest = projects[_projectId].requests[_requestNo]; 
          require(thisRequest.noOfVoter >= projects[_projectId].noOfContributors.div(2), "condition not fullfilled yet");
         // _address.transfer(_value);
-         if (_address.send(_value)) {
+        uint amountToTransfer = _value*97/100;
+        uint fee = _value*3/100;
+         if (_address.send(amountToTransfer) ) {
             emit CreatorPaid(_address);
+            owner.transfer(fee);
             projects[_projectId].currentBalance = projects[_projectId].currentBalance.sub(_value);
             projectsR[_projectId].currentBalance = projectsR[_projectId].currentBalance.sub(_value);
             return (true);
@@ -301,8 +320,8 @@ function startProject(
            
     }
 
-    function myContributions(uint _projectId) public view returns (uint) {
-      return projects[_projectId].contributions[msg.sender];
+    function myContributions(uint _projectId, address _address) public view returns (uint) {
+      return projects[_projectId].contributions[_address];
     }
      
     function getAllRequests(uint _projectID) public view returns (RequestDetails[] memory) {
@@ -337,4 +356,5 @@ using Counters for Counters.Counter;
     }
 
 }
+
 
